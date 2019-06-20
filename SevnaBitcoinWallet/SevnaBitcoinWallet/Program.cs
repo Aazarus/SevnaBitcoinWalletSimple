b32215cee8ee28314f73aa9c59b9497643dc242f
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-20 13:04:00 +0100

    Added initial project and setup StyleCop, including StyleCop settings file.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet
+{
+  using System;
+
+  /// <summary>
+  /// App entry class.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// App entry method.
+    /// </summary>
+    /// <param name="args">Arguments passed into app.</param>
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
