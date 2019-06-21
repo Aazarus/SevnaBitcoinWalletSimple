a88e4f1f2ef888631a0fca6d9602ce4a0388799c
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-21 14:39:14 +0100

    Added Custom Exception for not being able to match a Network for any reason. i.e. String to Network value.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/NetworkNoMatchException.cs
@@ -0,0 +1,36 @@
+﻿namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+
+  /// <summary>
+  /// Custom exception for no matching network.
+  /// </summary>
+  public class NetworkNoMatchException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NetworkNoMatchException"/> class.
+    /// </summary>
+    public NetworkNoMatchException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NetworkNoMatchException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public NetworkNoMatchException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NetworkNoMatchException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="inner">Next Exception.</param>
+    public NetworkNoMatchException(string message, Exception inner)
+      : base(message, inner)
+    {
+    }
+  }
+}
