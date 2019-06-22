af85a95baea2a55429cbeee2c97d1cb0ac04800b
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-22 20:02:09 +0100

    Added Commands collection with tests.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet.Tests/Commands/CommandsTests.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="CommandsTests.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Tests.Commands
+{
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using SevnaBitcoinWallet.Commands;
+  using Xunit;
+
+  /// <summary>
+  /// Tests the Commands class.
+  /// </summary>
+  public class CommandsTests
+  {
+    /// <summary>
+    /// Tests the Commands class accepts the correct commands.
+    /// </summary>
+    [Fact]
+    public void Commands_ShouldHoldCorrectAcceptedCommands()
+    {
+      // Arrange
+      var approvedCommands = new List<string>
+      {
+        "help",
+        "generate-wallet",
+        "recover-wallet",
+        "show-balances",
+        "show-history",
+        "receive",
+        "send",
+      };
+
+      // Act
+      // Assert
+      Commands.ApprovedCommands.Count.Should().Be(approvedCommands.Count);
+      Commands.ApprovedCommands.Should().BeEquivalentTo(approvedCommands);
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Commands/Commands.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Commands.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Commands
+{
+  using System.Collections.Generic;
+
+  /// <summary>
+  /// Defines commandline commands.
+  /// </summary>
+  public class Commands
+  {
+    /// <summary>
+    /// Gets the accepted Commandline commands.
+    /// </summary>
+    public static HashSet<string> ApprovedCommands { get; } = new HashSet<string>()
+    {
+      "help",
+      "generate-wallet",
+      "recover-wallet",
+      "show-balances",
+      "show-history",
+      "receive",
+      "send",
+    };
+  }
+}
