3d122dd2dd84af4e38f85f5f2f5383b80e2a4338
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-22 19:38:12 +0100

    Added Configuration and unit tests

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet.Tests/ConfigurationTests.cs
@@ -0,0 +1,121 @@
+﻿// <copyright file="ConfigurationTests.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Tests
+{
+  using System;
+  using System.IO;
+  using FluentAssertions;
+  using NBitcoin;
+  using Xunit;
+
+  /// <summary>
+  /// Tests the Configuration class.
+  /// </summary>
+  public class ConfigurationTests
+  {
+    /// <summary>
+    /// Tests the Save method correctly saves the information to file.
+    /// </summary>
+    [Fact]
+    public void Save_CorrectlySerializesInformationToFile()
+    {
+      // Arrange
+      const string fileName = "Save_CorrectlySerializesInformationToFile.json";
+      var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+
+      // Act
+      try
+      {
+        Configuration.Save(filePath);
+      }
+      catch (IOException ex)
+      {
+        // Default to a failing test, this should not happen.
+        ex.Should().BeNull();
+      }
+
+      // Assert
+      File.Exists(filePath).Should().BeTrue();
+
+      var results = File.ReadAllText(filePath);
+      results.Should().Be("{\"WalletFileName\":\"BitcoinWallet.json\",\"Network\":\"Main\",\"ConnectionType\":\"Http\",\"CanSpendUnconfirmed\":\"False\"}");
+
+      // Clean up
+      File.Delete(filePath);
+    }
+
+    /// <summary>
+    /// Tests the Load method correctly loads configuration from file when using the same hardcoded configuration values.
+    /// </summary>
+    [Fact]
+    public void Load_CorrectlyLoadsConfigurationDataFromFileWithDefaults()
+    {
+      // Arrange
+      const string fileName = "Load_CorrectlyLoadsConfigurationDataFromFile.json";
+      var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+
+      const string walletFileName = "BitcoinWallet.json";
+      var testNetwork = Network.Main;
+      const ConnectionType testConnectionType = ConnectionType.Http;
+      const bool testCanSpendUnconfirmed = false;
+      const string expectedFileContents = "{\"WalletFileName\":\"BitcoinWallet.json\",\"Network\":\"Main\",\"ConnectionType\":\"Http\",\"CanSpendUnconfirmed\":\"False\"}";
+
+      if (File.Exists(filePath))
+      {
+        File.Delete(filePath);
+      }
+
+      File.WriteAllText(filePath, expectedFileContents);
+
+      // Act
+      Configuration.Load(filePath);
+
+      // Assert
+      Configuration.DefaultWalletFileName.Should().Be(walletFileName);
+      Configuration.DefaultNetwork.Should().Be(testNetwork);
+      Configuration.DefaultConnectionType.Should().Be(testConnectionType);
+      Configuration.CanSpendUnconfirmed.Should().Be(testCanSpendUnconfirmed);
+
+      // Clean
+      File.Delete(filePath);
+    }
+
+    /// <summary>
+    /// Tests the Load method correctly loads configuration from file when using custom valid values.
+    /// </summary>
+    [Fact]
+    public void Load_CorrectlyLoadsConfigurationDataFromFileWithCustom()
+    {
+      // Arrange
+      const string fileName = "Load_CorrectlyLoadsConfigurationDataFromFile.json";
+      var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+
+      const string walletFileName = "TestWallet.json";
+      var testNetwork = Network.RegTest;
+      const ConnectionType testConnectionType = ConnectionType.FullNode;
+      const bool testCanSpendUnconfirmed = true;
+      const string expectedFileContents = "{\"WalletFileName\":\"TestWallet.json\",\"Network\":\"RegTest\",\"ConnectionType\":\"FullNode\",\"CanSpendUnconfirmed\":\"True\"}";
+
+      if (File.Exists(filePath))
+      {
+        File.Delete(filePath);
+      }
+
+      File.WriteAllText(filePath, expectedFileContents);
+
+      // Act
+      Configuration.Load(filePath);
+
+      // Assert
+      Configuration.DefaultWalletFileName.Should().Be(walletFileName);
+      Configuration.DefaultNetwork.Should().Be(testNetwork);
+      Configuration.DefaultConnectionType.Should().Be(testConnectionType);
+      Configuration.CanSpendUnconfirmed.Should().Be(testCanSpendUnconfirmed);
+
+      // Clean
+      File.Delete(filePath);
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Configuration.cs
@@ -0,0 +1,70 @@
+﻿// <copyright file="Configuration.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet
+{
+  using NBitcoin;
+  using SevnaBitcoinWallet.Extension;
+
+  /// <summary>
+  /// The following class creates a Configuration file which stores global settings.
+  /// </summary>
+  public static class Configuration
+  {
+    /// <summary>
+    /// Initializes static members of the <see cref="Configuration"/> class.
+    /// </summary>
+    static Configuration()
+    {
+    }
+
+    /// <summary>
+    /// Gets the default file name for a Bitcoin wallet.
+    /// </summary>
+    public static string DefaultWalletFileName { get; private set; } = @"BitcoinWallet.json";
+
+    /// <summary>
+    /// Gets the default Network for the Bitcoin wallet.
+    /// </summary>
+    public static Network DefaultNetwork { get; private set; } = Network.Main;
+
+    /// <summary>
+    /// Gets the default connection type for the Bitcoin wallet.
+    /// </summary>
+    public static ConnectionType DefaultConnectionType { get; private set; } = ConnectionType.Http;
+
+    /// <summary>
+    /// Gets a value indicating whether unconfirmed coins can be spent.
+    /// </summary>
+    public static bool CanSpendUnconfirmed { get; private set; } = false;
+
+    /// <summary>
+    /// Saves the Configuration information to file.
+    /// </summary>
+    /// <param name="configurationFileName">Client provided configuration file name.</param>
+    public static void Save(string configurationFileName = "")
+    {
+      ConfigurationFileSerializer.Serialize(
+        DefaultWalletFileName,
+        DefaultNetwork.ToString(),
+        DefaultConnectionType.ToString(),
+        CanSpendUnconfirmed.ToString(),
+        configurationFileName);
+    }
+
+    /// <summary>
+    /// Loads the Configuration information from file.
+    /// </summary>
+    /// <param name="filePath">Path to file.</param>
+    public static void Load(string filePath = "")
+    {
+      var configurationFileContents = ConfigurationFileSerializer.Deserialize(filePath);
+
+      DefaultWalletFileName = configurationFileContents.WalletFileName;
+      DefaultNetwork = DefaultNetwork.GetNetworkFromString(configurationFileContents.Network);
+      DefaultConnectionType = DefaultConnectionType.GetConnectionTypeFromString(configurationFileContents.ConnectionType);
+      CanSpendUnconfirmed = bool.Parse(configurationFileContents.CanSpendUnconfirmed);
+    }
+  }
+}
