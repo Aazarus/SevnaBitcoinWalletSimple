70359495d199ca4508d0b29bad4e6939e662ed4a
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-22 19:42:07 +0100

    Added Extension methods for Network and ConnectionType for string conversions with unit tests

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet.Tests/ExtensionMethodsTests.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="ExtensionMethodsTests.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Tests
+{
+  using System;
+  using FluentAssertions;
+  using NBitcoin;
+  using SevnaBitcoinWallet.Exceptions;
+  using SevnaBitcoinWallet.Extension;
+  using Xunit;
+
+  /// <summary>
+  /// Tests the ExtensionMethods class.
+  /// </summary>
+  public class ExtensionMethodsTests
+  {
+    /// <summary>
+    /// Tests the GetNetworkFromString extension method returns Main Network value.
+    /// </summary>
+    [Fact]
+    public void GetNetworkFromString_ShouldReturnCorrectNetworkValueMain()
+    {
+      // Arrange
+      const string networkAsString = "main";
+      var network = Network.RegTest;
+
+      // Act
+      network = network.GetNetworkFromString(networkAsString);
+
+      // Assert
+      network.Should().Be(Network.Main);
+    }
+
+    /// <summary>
+    /// Tests the GetNetworkFromString extension method returns RegTest Network value.
+    /// </summary>
+    [Fact]
+    public void GetNetworkFromString_ShouldReturnCorrectNetworkValueRegTest()
+    {
+      // Arrange
+      const string networkAsString = "RegTest";
+      var network = Network.Main;
+
+      // Act
+      network = network.GetNetworkFromString(networkAsString);
+
+      // Assert
+      network.Should().Be(Network.RegTest);
+    }
+
+    /// <summary>
+    /// Tests the GetNetworkFromString extension method returns TestNet Network value.
+    /// </summary>
+    [Fact]
+    public void GetNetworkFromString_ShouldReturnCorrectNetworkValueTestNet()
+    {
+      // Arrange
+      const string networkAsString = "TestNet";
+      var network = Network.Main;
+
+      // Act
+      network = network.GetNetworkFromString(networkAsString);
+
+      // Assert
+      network.Should().Be(Network.TestNet);
+    }
+
+    /// <summary>
+    /// Tests the GetNetworkFromString extension method returns TestNet Network value.
+    /// </summary>
+    [Fact]
+    public void GetNetworkFromString_ShouldThrowExceptionForUnknownNetwork()
+    {
+      // Arrange
+      const string networkAsString = "Fake";
+      var network = Network.Main;
+
+      try
+      {
+        // Act
+        network.GetNetworkFromString(networkAsString);
+      }
+      catch (NetworkNoMatchException ex)
+      {
+        // Assert
+        ex.Message.Should().Contain(networkAsString);
+      }
+      catch (Exception ex)
+      {
+        // Should not get here.
+        ex.Should().BeNull();
+      }
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Extension/ExtensionMethods.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="ExtensionMethods.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Extension
+{
+  using System;
+  using NBitcoin;
+  using SevnaBitcoinWallet.Exceptions;
+
+  /// <summary>
+  /// Defines extension methods.
+  /// </summary>
+  public static class ExtensionMethods
+  {
+    /// <summary>
+    /// Matches a string to a Network type.
+    /// </summary>
+    /// <param name="network">Class to add extension method to.</param>
+    /// <param name="parameters">The string representation of the Network.</param>
+    /// <returns>A Network value.</returns>
+    public static Network GetNetworkFromString(this Network network, params object[] parameters)
+    {
+      var networkToProcess = parameters[0].ToString();
+
+      switch (networkToProcess.ToLower())
+      {
+        case "main":
+          return Network.Main;
+
+        case "regtest":
+          return Network.RegTest;
+
+        case "testnet":
+          return Network.TestNet;
+
+        default:
+          throw new NetworkNoMatchException($"No match found for provided string representation of Network: {networkToProcess}");
+      }
+    }
+
+    /// <summary>
+    /// Matches a string to a ConnectionType.
+    /// </summary>
+    /// <param name="connectionType">Class to add extension method to.</param>
+    /// <param name="parameters">The string representation of the ConnectionType.</param>
+    /// <returns>A ConnectionType value.</returns>
+    public static ConnectionType GetConnectionTypeFromString(this ConnectionType connectionType, params object[] parameters)
+    {
+      var connectionTypeToProcess = parameters[0].ToString();
+
+      switch (connectionTypeToProcess.ToLower())
+      {
+        case "fullnode":
+          return ConnectionType.FullNode;
+        case "http":
+          return ConnectionType.Http;
+        default:
+          throw new ConnectionTypeNoMatchException($"No match found for provided string representation of ConnectionType: {connectionTypeToProcess}");
+      }
+    }
+  }
+}
