ba11936bebe2738733ae841f0c6028b5824230ec
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-24 12:19:10 +0100

    Added WalletManager and tests

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet.Tests/WalletManagerTests.cs
@@ -0,0 +1,250 @@
+﻿// <copyright file="WalletManagerTests.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using SevnaBitcoinWallet.Exceptions;
+  using Xunit;
+
+  /// <summary>
+  /// Tests the WalletManager class.
+  /// </summary>
+  public class WalletManagerTests
+  {
+    private static readonly string[] Args =
+    {
+      "recover-wallet", "wallet-file=test1.json",
+      "show-balances", "wallet-file=test1.json",
+      "send", "btc=0.0001", "address=mq6fK8fkFyCy9p53m4Gf4fiX2XCHvcwgi1", "wallet-file=test1.json",
+    };
+
+    /// <summary>
+    /// Tests the WalletManager constructor correctly adds CommandManager.
+    /// </summary>
+    [Fact]
+    public void WalletManager_ShouldEnsureConfigurationLoadOnInstantiation()
+    {
+      // Arrange
+      // Act
+      var walletManager = new WalletManager();
+      walletManager.AddCommands(Args);
+
+      // Assert
+      Configuration.DefaultWalletFileName.Should().NotBeNull();
+      Configuration.DefaultNetwork.Should().NotBeNull();
+      Configuration.DefaultConnectionType.Should().NotBeNull();
+    }
+
+    /// <summary>
+    /// Tests the WalletManager constructor correctly adds CommandManager.
+    /// </summary>
+    [Fact]
+    public void AddCommands_ShouldAddCommandsAsDefined()
+    {
+      // Arrange
+      var walletManager = new WalletManager();
+
+      // Act
+      walletManager.AddCommands(Args);
+
+      // Assert
+      walletManager.Commands.Should().BeEquivalentTo(Args);
+    }
+
+    /// <summary>
+    /// Tests the AddCommands method adds the provided commands to the end of the list.
+    /// </summary>
+    [Fact]
+    public void AddCommands_ShouldAddCommandsToEndOfCommandsCollection()
+    {
+      // Arrange
+      var walletManager = new WalletManager();
+      walletManager.AddCommands(Args);
+      var newCommand = new List<string>
+      {
+        "show-balances",
+        "wallet-file=test2.json",
+        "send", "btc=1.00", "address=mq6fK8fkFyCy9p53m4GG4fiE2XCKvcwgi4", "wallet-file=test2.json",
+      };
+
+      var expectedCommands = new List<string>();
+      expectedCommands.AddRange(Args);
+      expectedCommands.AddRange(newCommand);
+
+      // Act
+      walletManager.AddCommands(newCommand.ToArray());
+      var actualWalletManagerCommands = walletManager.Commands;
+
+      // Assert
+      actualWalletManagerCommands.Should().BeEquivalentTo(expectedCommands);
+    }
+
+    /// <summary>
+    /// Tests the AddCommands method throws an CommandArgumentNullOrEmptyException when no arguments provided.
+    /// </summary>
+    [Fact]
+    public void AddCommands_ShouldThrowExceptionForNullArguments()
+    {
+      // Arrange
+      // Act
+      try
+      {
+        var walletManager = new WalletManager();
+        walletManager.AddCommands(null);
+      }
+      catch (CommandArgumentNullOrEmptyException ex)
+      {
+        ex.Message.Should().Contain("Argument contains null or empty values.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here
+        ex.Should().BeNull();
+      }
+    }
+
+    /// <summary>
+    /// Tests the AddCommands method throws an CommandArgumentNullOrEmptyException when empty arguments are provided.
+    /// </summary>
+    [Fact]
+    public void AddCommands_ShouldThrowExceptionForEmptyArguments()
+    {
+      // Arrange
+      // Act
+      try
+      {
+        var commands = new[]
+        {
+          string.Empty,
+          string.Empty,
+        };
+        var walletManager = new WalletManager();
+        walletManager.AddCommands(commands);
+      }
+      catch (CommandArgumentNullOrEmptyException ex)
+      {
+        ex.Message.Should().Contain("Argument contains null or empty values.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here
+        ex.Should().BeNull();
+      }
+    }
+
+    /// <summary>
+    /// Tests the AddCommands method throws an CommandArgumentNullOrEmptyException when empty arguments are provided.
+    /// </summary>
+    [Fact]
+    public void AddCommands_ShouldThrowExceptionForArgumentsWithEmptyValues()
+    {
+      // Arrange
+      // Act
+      try
+      {
+        var commands = new[]
+        {
+          "show-balances",
+          string.Empty,
+        };
+        var walletManager = new WalletManager();
+        walletManager.AddCommands(commands);
+      }
+      catch (CommandArgumentNullOrEmptyException ex)
+      {
+        ex.Message.Should().Contain("Argument contains null or empty values.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here
+        ex.Should().BeNull();
+      }
+    }
+
+    /// <summary>
+    /// Tests the AddCommands method throws an CommandArgumentNullOrEmptyException when null arguments are provided.
+    /// </summary>
+    [Fact]
+    public void AddCommands_ShouldThrowExceptionForArgumentsWithNullValues()
+    {
+      // Arrange
+      // Act
+      try
+      {
+        var commands = new[]
+        {
+          "show-balances",
+          null,
+        };
+        var walletManager = new WalletManager();
+        walletManager.AddCommands(commands);
+      }
+      catch (CommandArgumentNullOrEmptyException ex)
+      {
+        ex.Message.Should().Contain("Argument contains null or empty values.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here
+        ex.Should().BeNull();
+      }
+    }
+
+    /// <summary>
+    /// Tests the ProcessCommands method throws CommandNotFoundException when no commands are available.
+    /// </summary>
+    [Fact]
+    public void ProcessCommands_ShouldThrowCustomExceptionWhenNoCommandsAvailable()
+    {
+      // Arrange
+      var walletManager = new WalletManager();
+      walletManager.Commands.Should().BeEmpty();
+
+      // Act
+      try
+      {
+        walletManager.ProcessCommands();
+      }
+      catch (CommandNotFoundException ex)
+      {
+        // Assert
+        ex.Message.Should().Be("No CommandManager found.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here
+        ex.Should().BeNull();
+      }
+    }
+
+    /// <summary>
+    /// Tests the ProcessCommands method throws CommandNotFoundException when no command is available.
+    /// </summary>
+    [Fact]
+    public void ProcessCommands_ShouldThrowCustomExceptionWhenCommandNotAvailable()
+    {
+      // Arrange
+      var walletManager = new WalletManager();
+
+      // Act
+      try
+      {
+        walletManager.ProcessCommand();
+      }
+      catch (CommandNotFoundException ex)
+      {
+        // Assert
+        ex.Message.Should().Be("No CommandManager Available.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here
+        ex.Should().BeNull();
+      }
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/WalletManager.cs
@@ -0,0 +1,134 @@
+﻿// <copyright file="WalletManager.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using SevnaBitcoinWallet.Commands;
+  using SevnaBitcoinWallet.Exceptions;
+
+  /// <summary>
+  /// The following class manages the Wallet.
+  /// </summary>
+  /// ToDo: Consider making this a singleton.
+  public sealed class WalletManager
+  {
+    /// <summary>
+    /// Lock token.
+    /// </summary>
+    private readonly object threadLock = new object();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletManager"/> class.
+    /// </summary>
+    public WalletManager()
+    {
+      this.Commands = new List<string>();
+      Configuration.Load();
+    }
+
+    /// <summary>
+    /// Gets the number of commands to process.
+    /// </summary>
+    /// ToDo: Improve this properties name, it is not just the commands. It is the commands and their corresponding arguments.
+    /// ToDo: Consider moving this to the CommandManager, doesn't make much sense that it is in this class.
+    public List<string> Commands { get; }
+
+    /// <summary>
+    /// Adds CommandManager to the list of CommandManager to process.
+    /// </summary>
+    /// <param name="argumentsToAdd">Arguments to add.</param>
+    /// <exception cref="CommandArgumentNullOrEmptyException">Null or Empty arguments were provided.</exception>
+    public void AddCommands(string[] argumentsToAdd)
+    {
+      if (ConfirmArgumentsAreValid(argumentsToAdd))
+      {
+        lock (this.threadLock)
+        {
+          this.Commands.AddRange(argumentsToAdd);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Processes each command in the List.
+    /// </summary>
+    /// <exception cref="CommandNotFoundException">CommandManager not found.</exception>
+    public void ProcessCommands()
+    {
+      while (this.CanContinueToProcessCommands())
+      {
+        try
+        {
+          this.ProcessCommand();
+        }
+        catch (CommandNotFoundException)
+        {
+          throw new CommandNotFoundException("No CommandManager found.");
+        }
+      }
+    }
+
+    /// <summary>
+    /// Processes a command.
+    /// </summary>
+    /// <exception cref="CommandNotFoundException">Command not found for index.</exception>
+    public void ProcessCommand()
+    {
+      if (this.Commands.Count == 0)
+      {
+        throw new CommandNotFoundException("No CommandManager Available.");
+      }
+
+      var identifiedCommandToProcess = this.GetCommandToProcess();
+      List<string> commandWithArguments = CommandManager.FindMatchingCommandWithArguments(identifiedCommandToProcess, this.Commands);
+    }
+
+    /// <summary>
+    /// Confirms the arguments are valid.
+    /// </summary>
+    /// <param name="argumentsToAdd">Arguments to check.</param>
+    /// <returns>True if valid else false.</returns>
+    /// <exception cref="CommandArgumentNullOrEmptyException">Null or Empty arguments were provided.</exception>
+    private static bool ConfirmArgumentsAreValid(IEnumerable<string> argumentsToAdd)
+    {
+      if (argumentsToAdd == null)
+      {
+        throw new CommandArgumentNullOrEmptyException("Argument contains null or empty values.");
+      }
+
+      var argumentsAsList = argumentsToAdd.ToList();
+      argumentsAsList.Find(string.IsNullOrEmpty);
+      if (argumentsAsList.Count == 0)
+      {
+        throw new CommandArgumentNullOrEmptyException("Argument contains null or empty values.");
+      }
+
+      return true;
+    }
+
+    /// <summary>
+    /// Returns the string for the command at the requested index.
+    /// </summary>
+    /// <returns>Command to process as string.</returns>
+    /// <exception cref="CommandNotFoundException">Command not found for index.</exception>
+    private string GetCommandToProcess()
+    {
+      return this.Commands.First() ?? throw new CommandNotFoundException("No Command found.");
+    }
+
+    /// <summary>
+    /// Checks if there are commands still available to process.
+    /// </summary>
+    /// <returns>Whether commands are still present.</returns>
+    private bool CanContinueToProcessCommands()
+    {
+      lock (this.threadLock)
+      {
+        return this.Commands.Count > 0;
+      }
+    }
+  }
+}
