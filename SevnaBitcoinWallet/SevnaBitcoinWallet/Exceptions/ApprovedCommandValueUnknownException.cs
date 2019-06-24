bf42e07689d7d10d50a932f7b8f0416846145e79
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-24 12:18:53 +0100

    Add additional custom exceptions.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/ApprovedCommandValueUnknownException.cs
@@ -0,0 +1,36 @@
+﻿namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+
+  /// <summary>
+  /// Custom exception for unknown ApprovedCommand value.
+  /// </summary>
+  public class ApprovedCommandValueUnknownException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApprovedCommandValueUnknownException"/> class.
+    /// </summary>
+    public ApprovedCommandValueUnknownException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApprovedCommandValueUnknownException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public ApprovedCommandValueUnknownException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApprovedCommandValueUnknownException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Next Exception.</param>
+    public ApprovedCommandValueUnknownException(string message, Exception innerException)
+      : base(message, innerException)
+    {
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/CommandArgumentNullOrEmptyException.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="CommandArgumentNullOrEmptyException.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+
+  /// <summary>
+  /// Custom exception for CommandManager provided as arguments.
+  /// </summary>
+  public class CommandArgumentNullOrEmptyException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommandArgumentNullOrEmptyException"/> class.
+    /// </summary>
+    public CommandArgumentNullOrEmptyException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommandArgumentNullOrEmptyException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public CommandArgumentNullOrEmptyException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommandArgumentNullOrEmptyException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Next Exception.</param>
+    public CommandArgumentNullOrEmptyException(string message, Exception innerException)
+      : base(message, innerException)
+    {
+    }
+  }
+}
