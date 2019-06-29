096d61b91436c0d3a28c9b04c8e7eba8ee85d794
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-29 12:27:59 +0100

    Added new custom exceptions.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/GenerateWalletFailedException.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="GenerateWalletFailedException.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+
+  /// <summary>
+  /// Custom exception for when wallet generation fails.
+  /// </summary>
+  public class GenerateWalletFailedException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenerateWalletFailedException"/> class.
+    /// </summary>
+    public GenerateWalletFailedException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenerateWalletFailedException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public GenerateWalletFailedException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenerateWalletFailedException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="inner">Next Exception.</param>
+    public GenerateWalletFailedException(string message, Exception inner)
+      : base(message, inner)
+    {
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/WalletAlreadyExistsException.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="WalletAlreadyExistsException.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+
+  /// <summary>
+  /// Custom exception for when a Wallet File already exists.
+  /// </summary>
+  public class WalletAlreadyExistsException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletAlreadyExistsException"/> class.
+    /// </summary>
+    public WalletAlreadyExistsException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletAlreadyExistsException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public WalletAlreadyExistsException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletAlreadyExistsException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="inner">Next Exception.</param>
+    public WalletAlreadyExistsException(string message, Exception inner)
+      : base(message, inner)
+    {
+    }
+  }
+}
