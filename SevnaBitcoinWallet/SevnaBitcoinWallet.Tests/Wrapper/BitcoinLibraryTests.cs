a3a4f2970fbd0e63e7e6092a88e141a33d3531e5
Aaron Waddingham <awaddingham@hotmail.com>
2019-06-29 12:32:19 +0100

    Added BitcoinLibrary to provide a layer between the bitcoin library and SevnaBitcoinWallet. Currently process Generate-Wallet. Tests included.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet.Tests/Wrapper/BitcoinLibraryTests.cs
@@ -0,0 +1,173 @@
+﻿// <copyright file="BitcoinLibraryTests.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Tests.Wrapper
+{
+  using System;
+  using System.IO;
+  using System.Security;
+  using FluentAssertions;
+  using SevnaBitcoinWallet.Exceptions;
+  using SevnaBitcoinWallet.Wrapper;
+  using Xunit;
+
+  /// <summary>
+  /// Tests the BitcoinLibrary class.
+  /// </summary>
+  public class BitcoinLibraryTests
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BitcoinLibraryTests"/> class.
+    /// </summary>
+    public BitcoinLibraryTests()
+    {
+      try
+      {
+        // Ensure Wallets directory doesn't already exist
+        Directory.Delete("Wallets", true);
+      }
+      catch (DirectoryNotFoundException)
+      {
+        // This is not a problem as we are deleting the directory anyway.
+      }
+    }
+
+    /// <summary>
+    /// Tests the GenerateWallet method successfully generates a wallet.
+    /// </summary>
+    [Fact]
+    public void ProcessGenerateWallet_ShouldSuccessfullyGenerateAWallet()
+    {
+      // Arrange
+      var args = new[]
+      {
+        "wallet-file=wallet.json",
+      };
+      var expectedPathToFile = Path.Combine("Wallets", "wallet.json");
+      var password = new SecureString();
+      password.AppendChar('p');
+      password.AppendChar('a');
+      password.AppendChar('s');
+      password.AppendChar('s');
+      password.AppendChar('w');
+      password.AppendChar('o');
+      password.AppendChar('r');
+      password.AppendChar('d');
+
+      // Act
+      var mnemonic = BitcoinLibrary.GenerateWallet(args, password);
+
+      // Assert
+      mnemonic.Should().NotBeNull();
+      mnemonic.Split(",").Length.Should().Be(12);
+      File.Exists(expectedPathToFile).Should().BeTrue();
+
+      // Cleanup
+      // Ensure Wallets directory doesn't already exist
+      Directory.Delete("Wallets", true);
+    }
+
+    /// <summary>
+    /// Tests the GenerateWallet method should throw an exception if the wallet file already exists.
+    /// </summary>
+    [Fact]
+    public void ProcessGenerateWallet_ShouldThrowAnExceptionIfWalletAlreadyExists()
+    {
+      // Arrange
+      const string walletFileName = "wallet_generate_test.json";
+      using (File.Create(walletFileName))
+      {
+      }
+
+      var args = new[]
+      {
+        $"wallet-file={walletFileName}",
+      };
+      var password = new SecureString();
+      password.AppendChar('p');
+      password.AppendChar('a');
+      password.AppendChar('s');
+      password.AppendChar('s');
+      password.AppendChar('w');
+      password.AppendChar('o');
+      password.AppendChar('r');
+      password.AppendChar('d');
+
+      try
+      {
+        // Act
+        BitcoinLibrary.GenerateWallet(args, password);
+      }
+      catch (WalletAlreadyExistsException ex)
+      {
+        // Assert
+        ex.Message.Should().Be("The request to generate a new wallet failed because the wallet already exists.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here.
+        ex.Should().BeNull();
+      }
+
+      // Cleanup
+      Directory.Delete("Wallets", true);
+      File.Delete(walletFileName);  // Default results in locally created wallet file.
+    }
+
+    /// <summary>
+    /// Tests the GenerateWallet method should throw an exception if the argument is invalid.
+    /// </summary>
+    [Fact]
+    public void ProcessGenerateWallet_ShouldThrowAnExceptionIfArgumentIsInvalid()
+    {
+      // Arrange
+      var args = new[]
+      {
+        "wallet-file=",
+      };
+      var password = new SecureString();
+      password.AppendChar('p');
+      password.AppendChar('a');
+      password.AppendChar('s');
+      password.AppendChar('s');
+      password.AppendChar('w');
+      password.AppendChar('o');
+      password.AppendChar('r');
+      password.AppendChar('d');
+
+      try
+      {
+        // Act
+        BitcoinLibrary.GenerateWallet(args, password);
+      }
+      catch (GenerateWalletFailedException ex)
+      {
+        // Assert
+        ex.Message.Should().Be("Could not generate wallet 'wallet-file' argument is invalid.");
+      }
+      catch (Exception ex)
+      {
+        // Should not get here.
+        ex.Should().BeNull();
+      }
+
+      // Cleanup
+      File.Delete("BitcoinWallet.json");
+    }
+
+    /// <summary>
+    /// Tests the ProcessShowHistory method correctly processes a valid Wallet File for address details.
+    /// </summary>
+    [Fact]
+    public void ProcessShowHistory_ShouldReturnTheDetailsOfAnAddressWhenGivenValidWalletFile()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(false);
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Wrapper/BitcoinLibrary.cs
@@ -0,0 +1,114 @@
+﻿// <copyright file="BitcoinLibrary.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Wrapper
+{
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+  using System.Linq;
+  using System.Security;
+  using HBitcoin.KeyManagement;
+  using SevnaBitcoinWallet.Exceptions;
+
+  /// <summary>
+  /// The following class adds a layer of abstraction between SevnaBitcoinWallet
+  /// and the in use 3rd party Bitcoin Library.
+  /// </summary>
+  public class BitcoinLibrary
+  {
+    /// <summary>
+    /// Identifies the command and forwards to correct method for processing.
+    /// </summary>
+    /// <param name="commandWithArguments">The command to process with any available arguments.</param>
+    /// <param name="additionalCommandArguments">Any additional arguments required by a command. i.e. SecureString for GenerateWallet.</param>
+    /// <returns>Result of running command.</returns>
+    public static string ProcessCommand(List<string> commandWithArguments, object additionalCommandArguments = null)
+    {
+      switch (commandWithArguments.First())
+      {
+        case "generate-wallet":
+          return GenerateWallet(commandWithArguments.Skip(1).ToArray(), additionalCommandArguments as SecureString);
+        default:
+          throw new CommandNotFoundException($"Requested command to process '{commandWithArguments.First()}' not found.");
+      }
+    }
+
+    /// <summary>
+    /// Generates a wallet using the provided argument.
+    /// </summary>
+    /// <param name="args">The arguments for the command.</param>
+    /// <param name="password">Bitcoin wallet password.</param>
+    /// <returns>Comma separated wallet mnemonic.</returns>
+    /// <exception cref="WalletAlreadyExistsException">The requested wallet name already exists.</exception>
+    /// <exception cref="GenerateWalletFailedException">Failed to generate wallet.</exception>
+    public static string GenerateWallet(string[] args, SecureString password)
+    {
+      try
+      {
+        var walletFilePath = GetWalletFilePath(args);
+
+        if (FileHelper.CheckFileExists(walletFilePath))
+        {
+          throw new WalletAlreadyExistsException("The request to generate a new wallet failed because the wallet already exists.");
+        }
+
+        Safe.Create(
+          out var mnemonic,
+          /* Note: From the Sevna boundary we want the password to be secure the library however takes a string so we must convert.*/
+          new System.Net.NetworkCredential(string.Empty, password).Password,
+          walletFilePath,
+          Configuration.DefaultNetwork);
+
+        return string.Join(",", mnemonic.Words);
+      }
+      catch (InvalidCommandArgumentFound)
+      {
+        // ToDo: Log
+        throw new GenerateWalletFailedException("Could not generate wallet 'wallet-file' argument is invalid.");
+      }
+    }
+
+    /// <summary>
+    /// Pulls the arguments value out of the full argument string.
+    /// </summary>
+    /// <param name="args">Arguments to search.</param>
+    /// <param name="argumentOfInterest">Name of the argument of interest.</param>
+    /// <param name="required">Is the argument required.</param>
+    /// <returns>The argument value.</returns>
+    /// <exception cref="InvalidCommandArgumentFound">Command argument is missing or missing a value.</exception>
+    // ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Local
+    private static string GetArgumentValue(IEnumerable<string> args, string argumentOfInterest, bool required = true)
+    {
+      var arg = args.FirstOrDefault(x => x.StartsWith(argumentOfInterest));
+      var argValue = arg?.Substring(arg.IndexOf("=", StringComparison.Ordinal) + 1);
+
+      if (required && string.IsNullOrEmpty(argValue))
+      {
+        throw new InvalidCommandArgumentFound($"Expected an argument value but was not received. Invalid for argument: {arg}");
+      }
+
+      return argValue;
+    }
+
+    /// <summary>
+    /// Ensures the Wallet File Path is suitable for the Library.
+    /// </summary>
+    /// <param name="args">The wallets file path.</param>
+    /// <returns>The wallets full file path.</returns>
+    private static string GetWalletFilePath(IEnumerable<string> args)
+    {
+      var walletFileName = GetArgumentValue(args, "wallet-file", false);
+
+      if (string.IsNullOrEmpty(walletFileName))
+      {
+        return Configuration.DefaultWalletFileName;
+      }
+
+      const string walletDirectoryName = "Wallets";
+      Directory.CreateDirectory(walletDirectoryName);
+      return Path.Combine(walletDirectoryName, walletFileName);
+    }
+  }
+}
