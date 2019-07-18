62452ba78a7cd8658443f1733d66605921641725
Aaron Waddingham <awaddingham@hotmail.com>
2019-07-18 20:29:01 +0100

    Split BitcoinLibrary out into an interface for better separation of concerns to allow dependency injection.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Wrapper/IBitcoinLibrary.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="IBitcoinLibrary.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Wrapper
+{
+  using System.Collections.Generic;
+  using System.Security;
+  using Exceptions;
+
+  /// <summary>
+  /// Defines the interface for the BitcoinLibrary.
+  /// </summary>
+  public interface IBitcoinLibrary
+  {
+    /// <summary>
+    /// Identifies the command and forwards to correct method for processing.
+    /// </summary>
+    /// <param name="commandWithArguments">The command to process with any available arguments.</param>
+    /// <param name="additionalCommandArguments">Any additional arguments required by a command. i.e. SecureString for GenerateWallet.</param>
+    /// <returns>Result of running command.</returns>
+    string ProcessCommand(List<string> commandWithArguments, object additionalCommandArguments = null);
+
+    /// <summary>
+    /// Generates a wallet using the provided argument.
+    /// </summary>
+    /// <param name="args">The arguments for the command.</param>
+    /// <param name="password">Bitcoin wallet password.</param>
+    /// <returns>Comma separated wallet mnemonic.</returns>
+    /// <exception cref="WalletAlreadyExistsException">The requested wallet name already exists.</exception>
+    /// <exception cref="GenerateWalletFailedException">Failed to generate wallet.</exception>
+    string GenerateWallet(string[] args, SecureString password);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    string ProcessShowWallet(string[] args);
+
+    /// <summary>
+    /// Recreates a wallet based on the provided mnemonic.
+    /// </summary>
+    /// <param name="args">[0]"wallet-file=*Wallet file name*", [1]"mnemonic=*Comma separated mnemonic*.</param>
+    /// <param name="password">SecureString password.</param>
+    /// <returns>Location of generated wallet.</returns>
+    /// <exception cref="WalletAlreadyExistsException">The requested wallet name already exists.</exception>
+    /// <exception cref="GenerateWalletFailedException">Failed to generate wallet.</exception>
+    string ProcessRecoverWallet(string[] args, SecureString password);
+  }
+}
