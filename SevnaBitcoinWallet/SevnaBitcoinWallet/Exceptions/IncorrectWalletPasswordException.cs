1b918d23e57fe403057dff1db0fbc0858a1a7c57
Aaron Waddingham <awaddingham@hotmail.com>
2019-07-22 16:41:27 +0100

    Added custom Exceptions

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/IncorrectWalletPasswordException.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="IncorrectWalletPasswordException.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+
+  public class IncorrectWalletPasswordException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IncorrectWalletPasswordException"/> class.
+    /// </summary>
+    public IncorrectWalletPasswordException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IncorrectWalletPasswordException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public IncorrectWalletPasswordException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IncorrectWalletPasswordException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Next Exception.</param>
+    public IncorrectWalletPasswordException(string message, Exception innerException)
+      : base(message, innerException)
+    {
+    }
+  }
+}
---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/WalletDecryptionFailedException.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="WalletDecryptionFailedException.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+
+  /// <summary>
+  /// Custom exception for when Wallet fails to decrypt.
+  /// </summary>
+  public class WalletDecryptionFailedException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletDecryptionFailedException"/> class.
+    /// </summary>
+    public WalletDecryptionFailedException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletDecryptionFailedException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public WalletDecryptionFailedException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletDecryptionFailedException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Next Exception.</param>
+    public WalletDecryptionFailedException(string message, Exception innerException)
+      : base(message, innerException)
+    {
+    }
+  }
+}
