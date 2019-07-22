ae96fb2f5b0bb93e04721238fd0ee68209ab1fd4
Aaron Waddingham <awaddingham@hotmail.com>
2019-07-22 14:09:20 +0100

    Added custom exception for missing wallet.

---
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/Exceptions/WalletNotFoundException.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="WalletNotFoundException.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace SevnaBitcoinWallet.Exceptions
+{
+  using System;
+
+  /// <summary>
+  /// Custom exception for when a given wallet isn't found.
+  /// </summary>
+  public class WalletNotFoundException : Exception
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletNotFoundException"/> class.
+    /// </summary>
+    public WalletNotFoundException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletNotFoundException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public WalletNotFoundException(string message)
+      : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WalletNotFoundException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="inner">Next Exception.</param>
+    public WalletNotFoundException(string message, Exception inner)
+      : base(message, inner)
+    {
+    }
+  }
+}
