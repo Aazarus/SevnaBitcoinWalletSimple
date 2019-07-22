984afa7072f8b1bbe3a9814be7523d35d5ef5403
Aaron Waddingham <awaddingham@hotmail.com>
2019-07-22 16:53:24 +0100

    Excluded 3rd party code from checks and style guide

--- SevnaBitcoinWallet/SevnaBitcoinWallet/QBitNinjaJutsus/AddressHistoryRecord.cs
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/QBitNinjaJutsus/AddressHistoryRecord.cs
@@ -1,43 +1,56 @@
-﻿using NBitcoin;
-using QBitNinja.Client.Models;
+﻿#pragma warning disable SA1200 // Using directives should be placed correctly
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NBitcoin;
+using QBitNinja.Client.Models;
 
 namespace DotNetWallet.QBitNinjaJutsus
 {
-	public class AddressHistoryRecord
-	{
-		public readonly BalanceOperation Operation;
-		public readonly BitcoinAddress Address;
 
-		public AddressHistoryRecord(BitcoinAddress address, BalanceOperation operation)
-		{
-			Address = address;
-			Operation = operation;
-		}
+#pragma warning restore SA1200 // Using directives should be placed correctly
+#pragma warning disable SA1520 // Use braces consistently
+#pragma warning disable SA1606 // Element documentation should have summary text
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1401 // Fields should be private
+  public class AddressHistoryRecord
+  {
+    public readonly BalanceOperation Operation;
+    public readonly BitcoinAddress Address;
 
-		public Money Amount
-		{
-			get
-			{
-				var amount = (from Coin coin in Operation.ReceivedCoins
-							  let address =
-								  coin.GetScriptCode().GetDestinationAddress(Address.Network)
-							  where address == Address
-							  select coin.Amount).Sum();
-				return (from Coin coin in Operation.SpentCoins
-						let address =
-							coin.GetScriptCode().GetDestinationAddress(Address.Network)
-						where address == Address
-						select coin)
-					.Aggregate(amount, (current, coin) => current - coin.Amount);
-			}
-		}
+    public AddressHistoryRecord(BitcoinAddress address, BalanceOperation operation)
+    {
+      this.Address = address;
+      this.Operation = operation;
+    }
 
-		public DateTimeOffset FirstSeen => Operation.FirstSeen;
-		public bool Confirmed => Operation.Confirmations > 0;
-		public uint256 TransactionId => Operation.TransactionId;
-	}
+    public Money Amount
+    {
+      get
+      {
+        var amount = (from Coin coin in this.Operation.ReceivedCoins
+                      let address =
+                        coin.GetScriptCode().GetDestinationAddress(this.Address.Network)
+                      where address == this.Address
+                      select coin.Amount).Sum();
+        return (from Coin coin in this.Operation.SpentCoins
+                let address =
+                  coin.GetScriptCode().GetDestinationAddress(this.Address.Network)
+                where address == this.Address
+                select coin)
+          .Aggregate(amount, (current, coin) => current - coin.Amount);
+      }
+    }
+
+    public DateTimeOffset FirstSeen => this.Operation.FirstSeen;
+
+    public bool Confirmed => this.Operation.Confirmations > 0;
+
+    public uint256 TransactionId => this.Operation.TransactionId;
+  }
 }
+#pragma warning restore SA1600 // Elements should be documented
+#pragma warning restore SA1520 // Use braces consistently
+#pragma warning restore SA1606 // Element documentation should have summary text
+#pragma warning restore SA1401 // Fields should be private
--- SevnaBitcoinWallet/SevnaBitcoinWallet/QBitNinjaJutsus/QBitNinjaJutsus.cs
+++ SevnaBitcoinWallet/SevnaBitcoinWallet/QBitNinjaJutsus/QBitNinjaJutsus.cs
@@ -1,153 +1,186 @@
-﻿using NBitcoin;
-using QBitNinja.Client;
-using QBitNinja.Client.Models;
+﻿#pragma warning disable SA1200 // Using directives should be placed correctly
 using System.Collections.Generic;
 using System.Linq;
 using HBitcoin.KeyManagement;
+using NBitcoin;
+using QBitNinja.Client;
+using QBitNinja.Client.Models;
+using SevnaBitcoinWallet;
 using static System.Console;
 
+#pragma warning restore SA1200 // Using directives should be placed correctly
+#pragma warning disable SA1520 // Use braces consistently
+#pragma warning disable SA1606 // Element documentation should have summary text
+#pragma warning disable SA1600 // Elements should be documented
 namespace DotNetWallet.QBitNinjaJutsus
 {
-    public static class QBitNinjaJutsus
+  public static class QBitNinjaJutsus
+  {
+    public static void GetBalances(IEnumerable<AddressHistoryRecord> addressHistoryRecords, out Money confirmedBalance, out Money unconfirmedBalance)
     {
-		public static void GetBalances(IEnumerable<AddressHistoryRecord> addressHistoryRecords, out Money confirmedBalance, out Money unconfirmedBalance)
-		{
-			confirmedBalance = Money.Zero;
-			unconfirmedBalance = Money.Zero;
-			foreach (var record in addressHistoryRecords)
-			{
-				if (record.Confirmed)
-					confirmedBalance += record.Amount;
-				else
-				{
-					unconfirmedBalance += record.Amount;
-				}
-			}
-		}
-		public static bool SelectCoins(ref HashSet<Coin> coinsToSpend, Money totalOutAmount, List<Coin> unspentCoins)
-		{
-			var haveEnough = false;
-			foreach (var coin in unspentCoins.OrderByDescending(x => x.Amount))
-			{
-				coinsToSpend.Add(coin);
-				// if doesn't reach amount, continue adding next coin
-				if (coinsToSpend.Sum(x => x.Amount) < totalOutAmount) continue;
-				else
-				{
-					haveEnough = true;
-					break;
-				}
-			}
+      confirmedBalance = Money.Zero;
+      unconfirmedBalance = Money.Zero;
+      foreach (var record in addressHistoryRecords)
+      {
+        if (record.Confirmed)
+          confirmedBalance += record.Amount;
+        else
+        {
+          unconfirmedBalance += record.Amount;
+        }
+      }
+    }
 
-			return haveEnough;
-		}
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="secrets"></param>
-		/// <returns>dictionary with coins and if confirmed</returns>
-		public static Dictionary<Coin, bool> GetUnspentCoins(IEnumerable<ISecret> secrets)
-		{
-			var unspentCoins = new Dictionary<Coin, bool>();
-			foreach (var secret in secrets)
-			{
-				var destination = secret.PrivateKey.ScriptPubKey.GetDestinationAddress(Config.Network);
+    public static bool SelectCoins(ref HashSet<Coin> coinsToSpend, Money totalOutAmount, List<Coin> unspentCoins)
+    {
+      var haveEnough = false;
+      foreach (var coin in unspentCoins.OrderByDescending(x => x.Amount))
+      {
+        coinsToSpend.Add(coin);
 
-				var client = new QBitNinjaClient(Config.Network);
-				var balanceModel = client.GetBalance(destination, unspentOnly: true).Result;
-				foreach (var operation in balanceModel.Operations)
-				{
-					foreach (var elem in operation.ReceivedCoins.Select(coin => coin as Coin))
-					{
-						unspentCoins.Add(elem, operation.Confirmations > 0);
-					}
-				}
-			}
+        // if doesn't reach amount, continue adding next coin
+        if (coinsToSpend.Sum(x => x.Amount) < totalOutAmount) continue;
+        else
+        {
+          haveEnough = true;
+          break;
+        }
+      }
 
-			return unspentCoins;
-		}
-		public static Dictionary<uint256, List<BalanceOperation>> GetOperationsPerTransactions(Dictionary<BitcoinAddress, List<BalanceOperation>> operationsPerAddresses)
-		{
-			// 1. Get all the unique operations
-			var opSet = new HashSet<BalanceOperation>();
-			foreach (var elem in operationsPerAddresses)
-				foreach (var op in elem.Value)
-					opSet.Add(op);
-			if (opSet.Count() == 0) Program.Exit("Wallet has no history yet.");
+      return haveEnough;
+    }
 
-			// 2. Get all operations, grouped by transactions
-			var operationsPerTransactions = new Dictionary<uint256, List<BalanceOperation>>();
-			foreach (var op in opSet)
-			{
-				var txId = op.TransactionId;
-				List<BalanceOperation> ol;
-				if (operationsPerTransactions.TryGetValue(txId, out ol))
-				{
-					ol.Add(op);
-					operationsPerTransactions[txId] = ol;
-				}
-				else operationsPerTransactions.Add(txId, new List<BalanceOperation> { op });
-			}
+    public static Dictionary<Coin, bool> GetUnspentCoins(IEnumerable<ISecret> secrets)
+    {
+      var unspentCoins = new Dictionary<Coin, bool>();
+      foreach (var secret in secrets)
+      {
+        var destination = secret.PrivateKey.ScriptPubKey.GetDestinationAddress(Configuration.DefaultNetwork);
 
-			return operationsPerTransactions;
-		}
-		public static Dictionary<BitcoinAddress, List<BalanceOperation>> QueryOperationsPerSafeAddresses(Safe safe, int minUnusedKeys = 7, HdPathType? hdPathType = null)
-		{
-			if (hdPathType == null)
-			{
-				Dictionary<BitcoinAddress, List<BalanceOperation>> operationsPerReceiveAddresses = QueryOperationsPerSafeAddresses(safe, 7, HdPathType.Receive);
-				Dictionary<BitcoinAddress, List<BalanceOperation>> operationsPerChangeAddresses = QueryOperationsPerSafeAddresses(safe, 7, HdPathType.Change);
+        var client = new QBitNinjaClient(Configuration.DefaultNetwork);
+        var balanceModel = client.GetBalance(destination, unspentOnly: true).Result;
+        foreach (var operation in balanceModel.Operations)
+        {
+          foreach (var elem in operation.ReceivedCoins.Select(coin => coin as Coin))
+          {
+            unspentCoins.Add(elem, operation.Confirmations > 0);
+          }
+        }
+      }
 
-				var operationsPerAllAddresses = new Dictionary<BitcoinAddress, List<BalanceOperation>>();
-				foreach (var elem in operationsPerReceiveAddresses)
-					operationsPerAllAddresses.Add(elem.Key, elem.Value);
-				foreach (var elem in operationsPerChangeAddresses)
-					operationsPerAllAddresses.Add(elem.Key, elem.Value);
-				return operationsPerAllAddresses;
-			}
+      return unspentCoins;
+    }
 
-			var addresses = safe.GetFirstNAddresses(minUnusedKeys, hdPathType.GetValueOrDefault());
-			//var addresses = FakeData.FakeSafe.GetFirstNAddresses(minUnusedKeys);
+    public static Dictionary<uint256, List<BalanceOperation>> GetOperationsPerTransactions(Dictionary<BitcoinAddress, List<BalanceOperation>> operationsPerAddresses)
+    {
+      // 1. Get all the unique operations
+      var opSet = new HashSet<BalanceOperation>();
+      foreach (var elem in operationsPerAddresses)
+      {
+        foreach (var op in elem.Value)
+        {
+          opSet.Add(op);
+        }
+      }
 
-			var operationsPerAddresses = new Dictionary<BitcoinAddress, List<BalanceOperation>>();
-			var unusedKeyCount = 0;
-			foreach (var elem in QueryOperationsPerAddresses(addresses))
-			{
-				operationsPerAddresses.Add(elem.Key, elem.Value);
-				if (elem.Value.Count == 0) unusedKeyCount++;
-			}
-			WriteLine($"{operationsPerAddresses.Count} {hdPathType} keys are processed.");
+      if (opSet.Count() == 0)
+      {
+        return null;
+      }
 
-			var startIndex = minUnusedKeys;
-			while (unusedKeyCount < minUnusedKeys)
-			{
-				addresses = new List<BitcoinAddress>();
-				for (int i = startIndex; i < startIndex + minUnusedKeys; i++)
-				{
-					addresses.Add(safe.GetAddress(i, hdPathType.GetValueOrDefault()));
-					//addresses.Add(FakeData.FakeSafe.GetAddress(i));
-				}
-				foreach (var elem in QueryOperationsPerAddresses(addresses))
-				{
-					operationsPerAddresses.Add(elem.Key, elem.Value);
-					if (elem.Value.Count == 0) unusedKeyCount++;
-				}
-				WriteLine($"{operationsPerAddresses.Count} {hdPathType} keys are processed.");
-				startIndex += minUnusedKeys;
-			}
+      // 2. Get all operations, grouped by transactions
+      var operationsPerTransactions = new Dictionary<uint256, List<BalanceOperation>>();
+      foreach (var op in opSet)
+      {
+        var txId = op.TransactionId;
+        List<BalanceOperation> ol;
+        if (operationsPerTransactions.TryGetValue(txId, out ol))
+        {
+          ol.Add(op);
+          operationsPerTransactions[txId] = ol;
+        }
+        else operationsPerTransactions.Add(txId, new List<BalanceOperation> { op });
+      }
 
-			return operationsPerAddresses;
-		}
-		public static Dictionary<BitcoinAddress, List<BalanceOperation>> QueryOperationsPerAddresses(IEnumerable<BitcoinAddress> addresses)
-		{
-			var operationsPerAddresses = new Dictionary<BitcoinAddress, List<BalanceOperation>>();
-			var client = new QBitNinjaClient(Config.Network);
-			foreach (var addr in addresses)
-			{
-				var operations = client.GetBalance(addr, unspentOnly: false).Result.Operations;
-				operationsPerAddresses.Add(addr, operations);
-			}
-			return operationsPerAddresses;
-		}
-	}
+      return operationsPerTransactions;
+    }
+
+    public static Dictionary<BitcoinAddress, List<BalanceOperation>> QueryOperationsPerSafeAddresses(Safe safe, int minUnusedKeys = 7, HdPathType? hdPathType = null)
+    {
+      if (hdPathType == null)
+      {
+        Dictionary<BitcoinAddress, List<BalanceOperation>> operationsPerReceiveAddresses = QueryOperationsPerSafeAddresses(safe, 7, HdPathType.Receive);
+        Dictionary<BitcoinAddress, List<BalanceOperation>> operationsPerChangeAddresses = QueryOperationsPerSafeAddresses(safe, 7, HdPathType.Change);
+
+        var operationsPerAllAddresses = new Dictionary<BitcoinAddress, List<BalanceOperation>>();
+        foreach (var elem in operationsPerReceiveAddresses)
+        { 
+          operationsPerAllAddresses.Add(elem.Key, elem.Value);
+      }
+
+        foreach (var elem in operationsPerChangeAddresses)
+        {
+          operationsPerAllAddresses.Add(elem.Key, elem.Value);
+        }
+
+        return operationsPerAllAddresses;
+      }
+
+      var addresses = safe.GetFirstNAddresses(minUnusedKeys, hdPathType.GetValueOrDefault());
+
+      var operationsPerAddresses = new Dictionary<BitcoinAddress, List<BalanceOperation>>();
+      var unusedKeyCount = 0;
+      foreach (var elem in QueryOperationsPerAddresses(addresses))
+      {
+        operationsPerAddresses.Add(elem.Key, elem.Value);
+        if (elem.Value.Count == 0)
+        {
+          unusedKeyCount++;
+        }
+      }
+
+      WriteLine($"{operationsPerAddresses.Count} {hdPathType} keys are processed.");
+
+      var startIndex = minUnusedKeys;
+      while (unusedKeyCount < minUnusedKeys)
+      {
+        addresses = new List<BitcoinAddress>();
+        for (int i = startIndex; i < startIndex + minUnusedKeys; i++)
+        {
+          addresses.Add(safe.GetAddress(i, hdPathType.GetValueOrDefault()));
+        }
+
+        foreach (var elem in QueryOperationsPerAddresses(addresses))
+        {
+          operationsPerAddresses.Add(elem.Key, elem.Value);
+          if (elem.Value.Count == 0)
+          {
+            unusedKeyCount++;
+          }
+        }
+
+        WriteLine($"{operationsPerAddresses.Count} {hdPathType} keys are processed.");
+        startIndex += minUnusedKeys;
+      }
+
+      return operationsPerAddresses;
+    }
+
+    public static Dictionary<BitcoinAddress, List<BalanceOperation>> QueryOperationsPerAddresses(IEnumerable<BitcoinAddress> addresses)
+    {
+      var operationsPerAddresses = new Dictionary<BitcoinAddress, List<BalanceOperation>>();
+      var client = new QBitNinjaClient(Configuration.DefaultNetwork);
+
+      foreach (var addr in addresses)
+      {
+        var operations = client.GetBalance(addr, unspentOnly: false).Result.Operations;
+        operationsPerAddresses.Add(addr, operations);
+      }
+
+      return operationsPerAddresses;
+    }
+  }
 }
+#pragma warning restore SA1600 // Elements should be documented
+#pragma warning restore SA1520 // Use braces consistently
+#pragma warning restore SA1606 // Element documentation should have summary text
